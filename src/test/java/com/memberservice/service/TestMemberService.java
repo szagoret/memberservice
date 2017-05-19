96b9170c5f2590ae9601006efcd02d5af43f5904
szagoret <szagoret@pentalog.fr>
2017-05-19 20:25:38 +0300

    Add test utils class

---
+++ src/test/java/com/memberservice/service/TestMemberService.java
@@ -0,0 +1,7 @@
+package com.memberservice.service;
+
+/**
+ * Created by szagoret on 19.05.2017.
+ */
+public class TestMemberService {
+}
---
+++ src/test/java/com/memberservice/utils/MemberTestUtils.java
@@ -0,0 +1,32 @@
+package com.memberservice.utils;
+
+import com.memberservice.entity.Member;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by szagoret
+ */
+public abstract class MemberTestUtils {
+
+
+    public static Member getMember(Long memberId) {
+        Member member = new Member();
+        member.setId(memberId);
+        member.setFirstName("Leon");
+        member.setLastName("Kluge");
+        member.setZipCode("01317");
+        member.setBirthday(getDate(1970, 0, 15));
+        return member;
+    }
+
+
+
+    private static Date getDate(int year, int month, int day) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(year, month, day);
+        return calendar.getTime();
+    }
+}
