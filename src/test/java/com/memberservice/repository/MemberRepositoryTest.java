ee1a961c1aa2f1cb77c092df494cafcd7b68f270
szagoret <szagoret@pentalog.fr>
2017-05-19 20:08:42 +0300

    Add test methods for delete memebrs

--- src/test/java/com/memberservice/repository/MemberRepositoryTest.java
+++ src/test/java/com/memberservice/repository/MemberRepositoryTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -20,21 +19,40 @@ import java.util.Optional;
 @DataJpaTest
 public class MemberRepositoryTest {
 
+    private Member member;
+
     @Autowired
     private TestEntityManager entityManager;
 
     @Autowired
     private MemberRepository memberRepository;
 
-    @Test
-    public void testFindMemberById() {
-        Member member = new Member();
+
+    @Before
+    public void insertMember() {
+        member = new Member();
         member.setFirstName("John");
         member = entityManager.persist(member);
+    }
 
+    @Test
+    public void testFindMemberById() {
         Optional<Member> resultMember = memberRepository.findById(member.getId());
 
         Assert.assertTrue(resultMember.isPresent());
         Assert.assertEquals(member.getId(), resultMember.get().getId());
+    }
+
+    @Test
+    public void testDeleteMemberById() {
+        int deleted = memberRepository.deleteMemberById(member.getId());
+
+        Assert.assertEquals(deleted, 1);
+    }
+
+    @Test
+    public void testDeleteNotExistingMember() {
+        int deleted = memberRepository.deleteMemberById(member.getId() + 1);
+        Assert.assertEquals(deleted, 0);
     }
 }
